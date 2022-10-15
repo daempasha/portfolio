b9f96b56a66d331e4d2b7a468dbf60aa6006ef53
Daem Pasha <daempasha@gmail.com>
2022-10-15 22:45:54 +0100

    Added tests

---
+++ frontend/__tests__/stickycontainer.test.tsx
@@ -0,0 +1,12 @@
+import { render, fireEvent, screen } from "@testing-library/react";
+import StickyContainer from "../components/StickyContainer/stickycontainer.component";
+
+test("can see container", async () => {
+  const container = render(
+    <StickyContainer>
+      <p>Container test</p>
+    </StickyContainer>
+  );
+
+  expect(container).toMatchSnapshot();
+});
