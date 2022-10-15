fd54562917cce04d688a055542b6c029a3961c44
Daem Pasha <daempasha@gmail.com>
2022-10-15 23:16:39 +0100

    Fixed problem with tests

--- frontend/__tests__/stickycontainer.test.tsx
+++ frontend/__tests__/stickycontainer.test.tsx
@@ -1,5 +1,5 @@
 import { render, fireEvent, screen } from "@testing-library/react";
-import StickyContainer from "../components/StickyContainer/stickycontainer.component";
+import StickyContainer from "@components/StickyContainer/stickycontainer.component";
 
 test("can see container", async () => {
   const container = render(
