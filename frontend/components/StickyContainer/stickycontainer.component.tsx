6014b352f83bc8176a51b1761c6a66ca2c786f2d
Daem Pasha <daempasha@gmail.com>
2022-10-16 14:45:40 +0100

    Fixed sticky container on mobile

--- frontend/components/StickyContainer/stickycontainer.component.tsx
+++ frontend/components/StickyContainer/stickycontainer.component.tsx
@@ -4,7 +4,7 @@ interface iStickyContainer {
 
 const StickyContainer = ({ children }: iStickyContainer) => {
     return (
-        <div className="mx-2 md:sticky md:top-16 bg-white dark:bg-gray-900 rounded-md p-5">
+        <div className="mx-auto w-fit md:w-full md:mx-2 md:sticky md:top-16 bg-white dark:bg-gray-900 rounded-md p-5">
             {children}
         </div>
     )
